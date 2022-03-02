bd69bbaddbe2bf17c1e69b4f410a38f5f2479c2a
João Victor <joaovss1992@gmail.com>
2022-03-02 14:39:14 -0300

    Inclusão do arquivo de números perfeitos

---
+++ TESTES-PROVAS/src/UFRPE/NumPerf.java
@@ -0,0 +1,36 @@
+package UFRPE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NumPerf {
+    public static void main(String[] args) {
+
+        List<Integer> divisores = new ArrayList<>();
+        Scanner perfeito = new Scanner(System.in);
+        System.out.println("Insira o número para verificar se é perfeito: ");
+        int tentativa = perfeito.nextInt();
+        perfeito.close();
+        if (tentativa % 2 != 0) {
+            System.out.println("O número " + tentativa + " é ímpar. Até hoje não se tem notícias de um número ímpar ser perfeito.");
+        } else {
+            for (int i = 1; i < tentativa; i++) {
+                if ((tentativa % i) == 0) {
+                    divisores.add(i);
+                }
+            }
+            System.out.println("Os divisores de "+ tentativa + " menores que ele são: " + divisores);
+            int soma = 0;
+            for (Integer i : divisores) {
+                soma += i.intValue();
+            } if (soma == tentativa) {
+                System.out.println("A soma dos divisores de " + tentativa + " é: " + soma);
+                System.out.println(tentativa + " é um número perfeito.");
+            } else {
+                System.out.println("A soma dos divisores de " + tentativa + " é: " + soma);
+                System.out.println(tentativa + " não é um número perfeito");
+            }
+        }
+    }
+}
